e4da18baa13ef43c219427bc75836f553cb5cee5
ImDyA <vasurman13@gmail.com>
2021-07-25 13:05:35 +0300

    УРАААААgit commit - m УРААААА!

--- JS-HomeWork-8/scrypt.js
+++ JS-HomeWork-8/scrypt.js
@@ -42,7 +42,7 @@ const sortObj2 = {
     oranges: 0,
     pears: 0,
 }
-const arr1 = ["a", "v", "a", "b", "b"];
+const arr1 = ["a", "v", "a", "b", "b", "y", "y", "y", "y"];
 const arr2 = [
     "apples",
     "oranges",
@@ -54,17 +54,51 @@ const arr2 = [
     "pears",
 ];
 
-sortObj.a = arr1.filter((a) => a == "a").length;
-sortObj.b = arr1.filter((a) => a == "b").length;
-sortObj.v = arr1.filter((a) => a == "v").length;
-console.log(sortObj);
+// sortObj.a = arr1.filter((a) => a == "a").length;
+// sortObj.b = arr1.filter((a) => a == "b").length;
+// sortObj.v = arr1.filter((a) => a == "v").length;
+// console.log(sortObj);
 
-sortObj2.apples = arr2.filter((a) => a == "apples").length;
-sortObj2.oranges = arr2.filter((a) => a == "oranges").length;
-sortObj2.pears = arr2.filter((a) => a == "pears").length;
-console.log(sortObj2)
+// sortObj2.apples = arr2.filter((a) => a == "apples").length;
+// sortObj2.oranges = arr2.filter((a) => a == "oranges").length;
+// sortObj2.pears = arr2.filter((a) => a == "pears").length;
+// console.log(sortObj2);
 
-// Задание 2 конец
+
+// const test = (b) => {
+//     let c = b.concat().reduce(function(previousValue, currentValue, index, array){
+//         let a = {};
+//         a[previousValue] = currentValue
+//         return a;
+//     })
+//     return c
+//     // sortObj.a = b.filter((a) => a == "a").length;
+//     // return sortObj;
+// }
+// console.log(test(arr1))
+// console.log(test(arr1));
+// let cvb = arr1.concat().reduce(function(previousValue, currentValue, index, array) {
+//     let a = {};
+//     a[currentValue] = currentValue;
+//     return a
+//   }, {});
+//   console.log(cvb)
+
+// var obj = arr1.reduce(function(acc, cur, a, b) {
+//     acc[arr1[a]] = b.filter((a) => a == cur).length
+//     return acc;
+//   }, {});
+//   console.log(obj);
+  const chudo = (arr) => {
+        let a = arr.reduce(function(acc, cur, a, b) {
+        acc[arr1[a]] = b.filter((a) => a == cur).length
+        return acc;
+    }, {});
+    return a;
+  }
+  console.log(chudo(arr1))
+
+// Задание 2 конец урааааа!!!
 
 
 
